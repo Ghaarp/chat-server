7d89f141f7df90b19d318e009aed7e732d769cc4
Ghaarp <xdmail@bk.ru>
2025-03-05 21:05:18 +0300

    Added empty server logic

---
+++ cmd/server/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	generated "github.com/Ghaarp/chat-server/pkg/chat_v1"
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	address = "localhost:"
+	port    = "3000"
+)
+
+type server struct {
+	generated.UnimplementedChatV1Server
+}
+
+func (serv *server) Create(context context.Context, in *generated.CreateRequest) (*generated.CreateResponse, error) {
+	log.Printf(color.GreenString("%v", in))
+
+	return &generated.CreateResponse{}, nil
+}
+
+func (serv *server) Delete(context context.Context, in *generated.DeleteRequest) (*generated.DeleteResponse, error) {
+	log.Printf(color.GreenString("%v", in))
+
+	return &generated.DeleteResponse{}, nil
+}
+
+func (serv *server) SendMessage(context context.Context, in *generated.SendMessageRequest) (*generated.SendMessageResponse, error) {
+	log.Printf(color.GreenString("%v", in))
+
+	return &generated.SendMessageResponse{}, nil
+}
+
+func main() {
+
+	listener, err := net.Listen("tcp", address+port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	serverObj := grpc.NewServer()
+	reflection.Register(serverObj)
+	a := &server{}
+	generated.RegisterChatV1Server(serverObj, a)
+
+	log.Printf("Server started om %v", listener.Addr())
+
+	if err := serverObj.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/Ghaarp/chat-server
 
 go 1.23.3
+
+require (
+	github.com/fatih/color v1.18.0
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+)
